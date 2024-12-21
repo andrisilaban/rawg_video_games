7739cf1149dc381f1205faefbdd4a4cd0fe8dcc5
andrisilaban <andriantosilaban@gmail.com>
2024-12-21 17:37:17 +0700

    Update Favorite Screen

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import '../constants/constants.dart';
 import '../main.dart';
 import '../providers/game_provider.dart';
+import 'game_detail_screen.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -42,52 +43,64 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
               final game = provider.favoriteGames[index];
               return Column(
                 children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 10),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.network(
-                            game.backgroundImage,
-                            width: 100,
-                            height: 100,
-                            fit: BoxFit.cover,
-                          ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              GameDetailScreen(gameId: game.id),
                         ),
-                        sw15,
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                game.name,
-                                style: themeProvider.isDarkMode
-                                    ? ts16BlackWhite
-                                    : ts16BlackBold,
-                              ),
-                              sh10,
-                              Text('Released date ${game.released}'),
-                              sh10,
-                              Row(
-                                children: [
-                                  const Icon(Icons.star, color: Colors.orange),
-                                  sw5,
-                                  Text('${game.rating}'),
-                                ],
-                              ),
-                            ],
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.network(
+                              game.backgroundImage,
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.delete, color: Colors.red),
-                          onPressed: () {
-                            provider.deleteFavorite(game.id);
-                          },
-                        )
-                      ],
+                          sw15,
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  game.name,
+                                  style: themeProvider.isDarkMode
+                                      ? ts16BlackWhite
+                                      : ts16BlackBold,
+                                ),
+                                sh10,
+                                Text('Released date ${game.released}'),
+                                sh10,
+                                Row(
+                                  children: [
+                                    const Icon(Icons.star,
+                                        color: Colors.orange),
+                                    sw5,
+                                    Text('${game.rating}'),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.red),
+                            onPressed: () {
+                              provider.deleteFavorite(game.id);
+                            },
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ],
